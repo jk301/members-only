2ccac27092d4184c62bfd7b128e02730c3ab80d0
junaid <jk7948677@gmail.com>
2026-08-15 18:06:14 +0530

    Routes for member, login, reg

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,6 +4,46 @@ function getMainPage (req, res) {
     res.render('index')
 }
 
+function getRegister (req, res) {
+    res.render('register')
+}
+
+function getLogin (req, res) {
+    res.render('login')
+}
+
+function getMemberCheck (req, res) {
+    res.render('member')
+}
+
+
+function postRegister (req, res) {
+    const password = req.body.password
+    const username = req.body.username
+    console.log('Register creds ->', username, password)
+    res.redirect('/')
+}
+
+function postLogin (req, res) {
+    const password = req.body.password
+    const username = req.body.username
+    console.log('Login creds ->', username, password)
+    res.redirect('/')
+}
+
+function postMemberCheck (req, res) {
+    console.log("do something with member stuff, pass submitted -> ", req.body.memPass)
+    res.redirect('/')
+}
+
+
+
 module.exports = {
-    getMainPage
+    getMainPage,
+    getRegister,
+    getLogin,
+    getMemberCheck,
+    postRegister,
+    postLogin,
+    postMemberCheck
 }
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,6 +6,12 @@ const indexController = require('../controllers/indexController.js')
 const indexRouter = Router()
 
 indexRouter.get('/', indexController.getMainPage)
+indexRouter.get('/register', indexController.getRegister)
+indexRouter.get('/login', indexController.getLogin)
+indexRouter.get('/member', indexController.getMemberCheck)
 
+indexRouter.post('/register', indexController.postRegister)
+indexRouter.post('/login', indexController.postLogin)
+indexRouter.post('/member', indexController.postMemberCheck)
 
 module.exports = indexRouter
