f320d20988e9de87ef7fe8c9118a91133e60eea8
junaid <jk7948677@gmail.com>
2026-08-20 14:50:09 +0530

    Added cookie parser & initialized passport

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const path = require('path')
 const passport = require('passport')
+const cookieParser = require('cookie-parser')
 
 require('dotenv').config()
 
@@ -11,8 +12,10 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-// gotta figure out what i need then open it up
-// require('./config/passport')(passport)
+app.use(cookieParser())
+
+app.use(passport.initialize())
+require('./config/passport')(passport)
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
