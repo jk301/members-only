e0c25aa0f647870ebc2de9b0e729a11f22c5f6fc
junaid <jk7948677@gmail.com>
2026-08-20 14:55:48 +0530

    Added proper pool props

--- db/db.js
+++ db/db.js
@@ -3,4 +3,10 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-module.exports = new Pool({});
+module.exports = new Pool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.DB_PORT
+});
