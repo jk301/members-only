499539deee66d6fbf76e198c3d1125e3033662c2
junaid <jk7948677@gmail.com>
2026-08-23 18:23:06 +0530

    Added message routes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -12,11 +12,15 @@ indexRouter.get('/register', indexController.getRegister)
 indexRouter.get('/login', indexController.getLogin)
 indexRouter.get('/member', passport.authenticate('jwt', { session: false }), indexController.getMemberCheck)
 indexRouter.get('/admin', passport.authenticate('jwt', { session: false }), indexController.getAdminCheck)
+indexRouter.get('/message', passport.authenticate('jwt', { session: false }), indexController.getMessage)
 
 indexRouter.post('/register', indexController.postRegister)
 indexRouter.post('/login', indexController.postLogin)
 indexRouter.post('/member', passport.authenticate('jwt', { session: false }),  indexController.postMemberCheck)
 indexRouter.post('/logout', indexController.postLogout)
 indexRouter.post('/admin', passport.authenticate('jwt', { session: false }),  indexController.postAdminCheck)
+indexRouter.post('/message', passport.authenticate('jwt', { session: false }), indexController.postMessage)
+
+indexRouter.post('/messages/delete/:id', passport.authenticate('jwt', { session: false }), indexController.deleteMessage)
 
 module.exports = indexRouter
