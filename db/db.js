5a91bb1da0e1c0fef0caec7786c2c5f21308cfd8
junaid <jk7948677@gmail.com>
2026-08-25 20:29:14 +0530

    Added conn string for hosted db

--- db/db.js
+++ db/db.js
@@ -4,9 +4,5 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 module.exports = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL, 
 });
