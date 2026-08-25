49682f421798b458795b93f292f55d62e78d4f2c
junaid <jk7948677@gmail.com>
2026-08-25 19:16:03 +0530

    Added public static dir

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+
 app.use(cookieParser())
 
 app.use(passport.initialize())
